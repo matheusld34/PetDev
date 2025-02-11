3db8ed19038b4c67c03d4edec95d3e66f6c9d644
Matheus <matheus_lopes151@hotmail.com>
2025-02-11 20:34:23 -0300

    hero

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -4,9 +4,9 @@ import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr"
 export function Hero() {
     return (
         <section className="bg-[#E84C3D] text-white relative overflow-hidden ">
-            <div>
-                <article>
-                    <div>
+            <div className="container mx-auto py-16 px-4 relative">
+                <article className="grid grid-cols-1 lg:grid-cols-2 gap-8 ">
+                    <div className="space-y-6">
                         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10">
                             Seu pet merece cuidado, carinho e atenção especial
                         </h1>
@@ -24,6 +24,9 @@ export function Hero() {
                                 <b className="bg-black text-white px-2 py-1 rounded-md"> 5% </b>de desconto na primeira compra
                             </p>
                         </div>
+                    </div>
+                    <div>
+                        <h1>Foto do cachorro</h1>
                     </div>
                 </article>
             </div>
