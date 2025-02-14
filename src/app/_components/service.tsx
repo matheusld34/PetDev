5aa4267d2ca5807c81ec16fde34aea261f76783e
Matheus <matheus_lopes151@hotmail.com>
2025-02-13 21:15:13 -0300

    Sobre finalizado

---
+++ src/app/_components/service.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import useEmblaCarousel from "embla-carousel-react";
+import { ChevronLeft, ChevronRight, Scissors, Syringe, CarTaxiFront, Hotel } from "lucide-react";
+import { WhatsappLogo } from "@phosphor-icons/react";
+
+export function Services() {
+    return (
+        <section className="bg-white py-16">
+            <div className="conatiner mx-auto px-4">
+                <div>
+                    <h2 className="font-4xl font-bold mb-12">Serviços</h2>
+
+                    <div className="relative">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import { Button } from "@/components/ui/button"
 import { Hero } from "./_components/hero"
 import { About } from "./_components/about"
-
+import { Services } from "./_components/service"
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Services />
     </main>
   )
 }
